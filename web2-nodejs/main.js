26f2a357ee0850d3e96e72d5788818a6f3599b28
Kim Jihoon <kimjihun1001@gmail.com>
2022-06-24 15:49:09 +0900

    리팩토링: 객체, 모듈화

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -2,45 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var path = require('path');
 
-function templateHTML(title, list, body, control){
-    return`
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB2</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    
-    `;
-}
-
-function templateList(filelist){
-    //     var list = `<ol>
-    //     <li><a href="/?id=HTML">HTML</a></li>
-    //     <li><a href="/?id=CSS">CSS</a></li>
-    //     <li><a href="/?id=JavaScript">JavaScript</a></li>
-    //   </ol>`
-
-    var list = '<ul>';
-
-    var i = 0;
-    while (i < filelist.length) {
-        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-        i = i + 1;
-    }
-
-    list = list + '</ul>';
-
-    return list;
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -53,24 +17,31 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data', (err, filelist) => {
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
-                var list = templateList(filelist);
-                var template = templateHTML(title, list, 
+                var list = template.list(filelist);
+                var html = template.html(title, list, 
                     `<h2>${title}</h2>${description}`,
                     `<a href="/create">create</a>`);
                 response.writeHead(200);
-                response.end(template); // 프로그래밍적으로 사용자에게 전송할 데이터를 생성한다. 기존 아파치 서버로는 할 수 없다.
+                response.end(html); // 프로그래밍적으로 사용자에게 전송할 데이터를 생성한다. 기존 아파치 서버로는 할 수 없다.
             })
 
         } else {
             fs.readdir('./data', (err, filelist) => {
-                fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-                    var title = queryData.id;
-                    var list = templateList(filelist);
-                    var template = templateHTML(title, list, 
+                // 이상한 접근을 필터링하기 위해서 base 부분만 가져옴. (파일명.확장자)
+                var filteredId = path.parse(queryData.id).base;
+                fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+                    var title = filteredId;
+                    var list = template.list(filelist);
+                    var html = template.html(title, list, 
                         `<h2>${title}</h2>${description}`,
-                        `<a href="/create">create</a><a href="/update?id=${title}">update</a>`);
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>`);
                     response.writeHead(200);
-                    response.end(template);
+                    response.end(html);
                 })
             })
         }
@@ -78,8 +49,8 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/create'){
         fs.readdir('./data', (err, filelist) => {
             var title = 'WEB - create';
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `
+            var list = template.list(filelist);
+            var html = template.html(title, list, `
                 <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
                     <p>
@@ -92,7 +63,7 @@ var app = http.createServer(function(request,response){
         
             `, '');
             response.writeHead(200);
-            response.end(template); 
+            response.end(html); 
         })
     } else if(pathname === '/create_process'){
         console.log(request.method);    //? 나중에 RESTful API 형식으로 만들려면 같은 URL을 사용하면서 method가 POST냐 GET이냐에 따라서 처리가 달라져야 할텐데. 그걸 이 조건문 걸어서 하면 되려나?
@@ -107,16 +78,17 @@ var app = http.createServer(function(request,response){
             var title = post.title;
             var description = post.description;
             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-                response.writeHead(302, {Location: `/?id=${title}`})
-                response.end('success');
+                response.writeHead(302, {Location: `/?id=${title}`})    //리다이렉션
+                response.end();
             })
         })
     } else if(pathname === '/update'){
         fs.readdir('./data', (err, filelist) => {
-            fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-                var title = queryData.id;
-                var list = templateList(filelist);
-                var template = templateHTML(title, list, 
+            var filteredId = path.parse(queryData.id).base;
+            fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+                var title = filteredId;
+                var list = template.list(filelist);
+                var html = template.html(title, list, 
                     `
                     <form action="/update_process" method="post">
                         <input type="hidden" name="id" value="${title}">
@@ -131,7 +103,7 @@ var app = http.createServer(function(request,response){
                     `,
                     `<a href="/create">create</a><a href="/update?id=${title}">update</a>`);
                 response.writeHead(200);
-                response.end(template);
+                response.end(html);
             })
         })
     } else if(pathname === '/update_process'){
@@ -148,9 +120,22 @@ var app = http.createServer(function(request,response){
             fs.rename(`data/${id}`, `data/${title}`,function(err){
                 fs.writeFile(`data/${title}`, description, 'utf8', function(err){
                     response.writeHead(302, {Location: `/?id=${title}`})
-                    response.end('success');
+                    response.end();
                 });
             });
+        })
+    } else if(pathname === '/delete_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        })
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(err){
+                response.writeHead(302, {Location: `/`})    //리다이렉션
+                response.end();
+            })
         })
     } else {
         response.writeHead(404);
